9f79ddd8d1449af475abbc1074f1851dbc1370dd
Samer <samerbenmim@insat.u-carthage.tn>
2022-06-27 11:06:29 +0100

    add coin

--- EssCoin.py
+++ EssCoin.py
@@ -114,7 +114,7 @@ def mine_block():
     previous_proof = previous_block['proof']
     proof = blockchain.proof_of_work(previous_proof)
     previous_hash = blockchain.hash(previous_block)
-    blockchain.add_transaction(sender = node_address, receiver = 'Samer', amount = 1)
+    blockchain.add_transaction(sender = node_address, receiver = 'Hadelin', amount = 1)
     block = blockchain.create_block(proof, previous_hash)
     response = {'message': 'Congratulations, you just mined a block!',
                 'index': block['index'],
@@ -180,6 +180,4 @@ def replace_chain():
     return jsonify(response), 200
 
 # Running the app
-#app.run(host = '0.0.0.0', port = 5000)
-
-print(blockchain.chain)
+# app.run(host = '0.0.0.0', port = 5000)
